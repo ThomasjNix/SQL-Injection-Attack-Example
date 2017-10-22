40787fd980ef2d436776399353fc6424eac2be0d
Thomas Nix <tnix2@uncc.edu>
2017-10-22 10:51:29 -0400

    Added AJAX request and append to html table.

--- public/js/ajax_request.js
+++ public/js/ajax_request.js
@@ -1,12 +1,26 @@
-$('form').on('submit', function(event){
-    event.preventDefault();
-     $.ajax({
-      type: 'POST',
-      url: '/query',
-      data: $("form").serialize(), 
-      //or your custom data either as object {foo: "bar", ...} or foo=bar&...
-      success: function(response) {
-        console.log(response);
-      },
+$(document).ready(function(){
+    // Load in table and create string to append
+    var tableBody = $("table > tbody");
+    var appendRow = "";
+    $('form').on('submit', function(event){
+        event.preventDefault();
+        
+        // Clear any previous table information
+        tableBody.empty();
+         $.ajax({
+          type: 'POST',
+          url: '/query',
+          data: $("form").serialize(), 
+          success: function(response) {
+            // Since this AJAX returns an array, we can loop through it and append items to the page  
+            $.each(response, function(index, item){
+                
+                // Append to table on successful data retrieve
+                appendRow = "<tr><td>"+item.name+"</td><td>$"+item.price+"</td><td>"+item.quantity+"</td></tr>"
+                tableBody.append(appendRow);
+            });
+          },
+        });
     });
-});
+
+})
